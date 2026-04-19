b2088b39a0f8e7ece5a0ee6f968d542038aa033b
Ashlesh V. Nayak <ashleshvnayak77@gmail.com>
2026-04-19 11:25:58 +0530

    Update ml_model.py

--- backend/ml_model.py
+++ backend/ml_model.py
@@ -1,1 +1,11 @@
+from sentence_transformers import SentenceTransformer, util
 
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def get_similarity(resume_text, job_description):
+    resume_emb = model.encode(resume_text)
+    jd_emb = model.encode(job_description)
+
+    score = util.cos_sim(resume_emb, jd_emb)
+
+    return float(score[0][0]) * 100
