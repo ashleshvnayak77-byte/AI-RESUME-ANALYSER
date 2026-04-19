39bbf71fe644b69f85f9ce4e596d2065bedec207
Ashlesh V. Nayak <ashleshvnayak77@gmail.com>
2026-04-19 11:28:54 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,20 @@
+document.getElementById("uploadForm").addEventListener("submit", async function(e) {
+    e.preventDefault();
 
+    let fileInput = document.getElementById("resume").files[0];
+    let jd = document.getElementById("jd").value;
+
+    let formData = new FormData();
+    formData.append("resume", fileInput);
+    formData.append("job_description", jd);
+
+    let response = await fetch("http://127.0.0.1:5000/analyze", {
+        method: "POST",
+        body: formData
+    });
+
+    let data = await response.json();
+
+    document.getElementById("result").innerHTML =
+        "Match Score: " + data.match_score + "%<br>Skills: " + data.skills.join(", ");
+});
